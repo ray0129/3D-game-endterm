c7e9715cf69fc4ef1c377c4c89bc30bc13e29acc
tomsangotw <tomsangtw@gmail.com>
2017-11-27 04:59:30 +0800

    add mines and try to merge

---
+++ Assets/Scripts/MineManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineManager : MonoBehaviour {
+
+	public static Transform[,] mines;//static to access any where
+
+	public Color temp;
+
+	public static int ColRow;
+
+	void Awake(){
+		ColRow = (int)Mathf.Sqrt (transform.childCount);
+		mines = new Transform[ColRow,ColRow]; //make the array for wavepoints
+		for (int i = 0; i < ColRow; i++) {
+			for (int j = 0; j < ColRow; j++) {
+				mines [i,j] = transform.GetChild (i*ColRow + j);
+				mines [i,j].GetComponent<MineMap> ().x = i;
+				mines [i,j].GetComponent<MineMap> ().y = j;
+			}
+		}
+		Debug.Log ("mine count = " + mines.Length.ToString ());
+
+
+		//test for array;
+		mines [0,0].GetComponent<Renderer> ().material.color = temp;
+		mines [0,1].GetComponent<Renderer> ().material.color = temp;
+		mines [0,2].GetComponent<Renderer> ().material.color = temp;
+		mines [0,4].GetComponent<Renderer> ().material.color = temp;
+
+	}
+}
---
+++ Assets/Scripts/MineMap.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineMap : MonoBehaviour {
+
+	public Color hoverColor;
+	public Color Mine;
+
+	private Renderer rend;
+
+	public bool itsMine;
+
+	public int x;
+	public int y;
+
+	void Start(){
+
+		itsMine = Random.value < 0.15; //random generate mines
+		rend = GetComponent<Renderer> ();
+
+	/*
+		for (int i = 0; i < MineManager.ColRow; i++) {
+			for (int j = 0; j < MineManager.ColRow; j++) {
+				if (MineManager.mines [i, j] == this) {
+					x = i;
+					y = j;
+				}
+			}
+		}*/
+
+	}
+
+	void OnMouseDown(){ //will need to modify to OnClosion or OnTrigger
+		Debug.Log("Find myself at:" + x.ToString() + " " + y.ToString() );
+		if (itsMine) {
+			rend.material.color = Mine; //change to safe color
+		}else {
+			rend.material.color = hoverColor;//change to mine color
+			//hp or sth go down
+		}
+	}
+
+
+}
