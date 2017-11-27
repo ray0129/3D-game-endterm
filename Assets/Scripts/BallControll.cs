916f71f3192f2e028a471b9e1368eb91ee9885c3
tomsangotw <tomsangtw@gmail.com>
2017-11-28 03:00:12 +0800

    update 35 throw ball

---
+++ Assets/Scripts/BallControll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControll : MonoBehaviour {
+	public GameObject balls;
+	public float time;
+	public float Destroy_time = 3;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time > 0.5f) {
+			if (Input.GetKeyUp (KeyCode.Mouse0)) {
+				Destroy(Instantiate (balls, this.transform.position, this.transform.rotation),Destroy_time);
+				time = 0f;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/BallFly.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFly : MonoBehaviour {
+	public float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate(new Vector3(0,0,speed));
+	}
+	private void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "Constraint") {
+			Destroy (this.gameObject);
+		}
+		if (collision.gameObject.tag == "Cube") {
+			Destroy (this.gameObject);
+			Destroy (collision.gameObject);
+		}
+	}
+}
