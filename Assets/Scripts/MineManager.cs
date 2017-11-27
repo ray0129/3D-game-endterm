d27f93a863b2e4029309895bf926393dc545502e
tomsangotw <tomsangtw@gmail.com>
2017-11-27 21:28:48 +0800

    - mineMaps recursive function to find all near blocks -

--- Assets/Scripts/MineManager.cs
+++ Assets/Scripts/MineManager.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class MineManager : MonoBehaviour {
 
 	public static Transform[,] mines;//static to access any where
+	public static bool[,] used;
 
-	public Color temp;
+	public Color Safe;
+	public Color Mine;
+
 
 	public static int ColRow;
 
 	void Awake(){
 		ColRow = (int)Mathf.Sqrt (transform.childCount);
 		mines = new Transform[ColRow,ColRow]; //make the array for wavepoints
+		used = new bool[ColRow,ColRow];
+
 		for (int i = 0; i < ColRow; i++) {
 			for (int j = 0; j < ColRow; j++) {
+				used [i,j] = false;
 				mines [i,j] = transform.GetChild (i*ColRow + j);
 				mines [i,j].GetComponent<MineMap> ().x = i;
 				mines [i,j].GetComponent<MineMap> ().y = j;
@@ -30,4 +36,30 @@ public class MineManager : MonoBehaviour {
 		mines [0,4].GetComponent<Renderer> ().material.color = temp;
 		*/
 	}
+
+	/*
+	public void recursive(int i, int j){
+		MM.used [i, j] = true;
+
+		if (MineManager.mines[i,j].GetComponent<MineMap>().itsMine) {
+			MineManager.mines[i,j].GetComponent<Renderer>().material.color = MineManager.mines[i,j].GetComponent<MineMap>().Mine; //change to mine color
+		}else {
+			MineManager.mines[i,j].GetComponent<Renderer>().material.color = MineManager.mines[i,j].GetComponent<MineMap>().hoverColor;//change to safe color
+			if (i > 0 && MineManager.used [i - 1, j] == false) {
+				recursive (i - 1, j); //down
+			}
+			if (j > 0 && MineManager.used [i, j - 1] == false) {
+				recursive (i, j - 1); //go left
+			}
+			if (i < MineManager.ColRow - 1 && MineManager.used [i + 1, j] == false) { //go up
+				recursive (i + 1, j);
+			}
+			if (i < MineManager.ColRow - 1 && MineManager.used [i, j + 1] == false) { //go right
+				recursive (i, j + 1);
+			}
+		}
+
+	}*/
+
 }
+
--- Assets/Scripts/MineMap.cs
+++ Assets/Scripts/MineMap.cs
@@ -19,26 +19,35 @@ public class MineMap : MonoBehaviour {
 		itsMine = Random.value < 0.15; //random generate mines
 		rend = GetComponent<Renderer> ();
 
-	/*
-		for (int i = 0; i < MineManager.ColRow; i++) {
-			for (int j = 0; j < MineManager.ColRow; j++) {
-				if (MineManager.mines [i, j] == this) {
-					x = i;
-					y = j;
-				}
-			}
-		}*/
-
 	}
 
 	void OnMouseDown(){ //will need to modify to OnClosion or OnTrigger
 		Debug.Log("Find myself at:" + x.ToString() + " " + y.ToString() );
-		if (itsMine) {
-			rend.material.color = Mine; //change to safe color
+
+		recursive (x, y);
+	}
+
+	void recursive(int i, int j){
+		MineManager.used [i, j] = true;
+
+		if (MineManager.mines[i,j].GetComponent<MineMap>().itsMine) {
+			MineManager.mines[i,j].GetComponent<Renderer>().material.color = MineManager.mines[i,j].GetComponent<MineMap>().Mine; //change to mine color
 		}else {
-			rend.material.color = hoverColor;//change to mine color
-			//hp or sth go down
+			MineManager.mines[i,j].GetComponent<Renderer>().material.color = MineManager.mines[i,j].GetComponent<MineMap>().hoverColor;//change to safe color
+			if (i > 0 && MineManager.used [i - 1, j] == false) {
+				recursive (i - 1, j); //down
+			}
+			if (j > 0 && MineManager.used [i, j - 1] == false) {
+				recursive (i, j - 1); //go left
+			}
+			if (i < MineManager.ColRow - 1 && MineManager.used [i + 1, j] == false) { //go up
+				recursive (i + 1, j);
+			}
+			if (j < MineManager.ColRow - 1 && MineManager.used [i, j + 1] == false) { //go right
+				recursive (i, j + 1);
+			}
 		}
+
 	}
 
 
