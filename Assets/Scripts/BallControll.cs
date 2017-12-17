18c23d77762412c9f3f8f1bf8b64bdf2f111479a
Lohan <Lohan@github.com>
2017-12-18 00:43:18 +0800

    direction

--- Assets/Scripts/BallControll.cs
+++ Assets/Scripts/BallControll.cs
@@ -6,7 +6,8 @@ public class BallControll : MonoBehaviour {
 	public GameObject balls;
 	public float time;
 	public float Destroy_time = 3;
-	public GameObject cam;
+	public GameObject direction;
+	private int x=0;
 	// Use this for initialization
 	void Start () {
 
@@ -14,11 +15,14 @@ public class BallControll : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		int j = Random.Range (0, 7);
 		time += Time.deltaTime;
 		if (time > 0.5f) {
 			if (Input.GetKeyUp (KeyCode.Mouse0)) {
+				direction.transform.Rotate (0, 0,(j-x)*45);
+				Debug.Log(j);
+				x = j;
 				Destroy(Instantiate (balls, this.transform.position, this.transform.rotation),Destroy_time);
-				time = 0f;
 			}
 		}
 	}
