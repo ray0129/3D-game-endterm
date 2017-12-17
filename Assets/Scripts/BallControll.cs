310efca6be79593cce74a02152eaedccf83bd6aa
tomsangotw <tomsangtw@gmail.com>
2017-12-18 02:39:18 +0800

    add turns -add UI

--- Assets/Scripts/BallControll.cs
+++ Assets/Scripts/BallControll.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class BallControll : MonoBehaviour {
 	public GameObject balls;
 	public float time;
@@ -9,17 +10,35 @@ public class BallControll : MonoBehaviour {
 	public float maxSpeed = 150f;
 	public float minSpeed = 3f;
 	public float cSpeed = 0f;
-	public float increase_speed = 25f;
+	public float increase_speed = 50f;
 	public GameObject cam;
 	public Slider slider;
+
+	//wind
+	public static float WindX; //range 0.1~1f
+	public static float WindY;
+	public static float WindZ;
+	//turn & gameControl
+	private bool isGameStart = false;
+	private bool isShooting = false;
+	public int TurnCount = 10;
+	public float TurnTime = 15;
+	private float currentTime;
+	private float temp;
+	public Text TimeCountDownText;
+	public Text ArrowRemainText;
+
 	// Use this for initialization
 	void Start () {
 		slider.value = 0;
+
+		isGameStart = false;
+		currentTime = TurnTime;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		time += Time.deltaTime;
+	/*	time += Time.deltaTime;
 		if (time > 0.5f) {
 			if (Input.GetMouseButton (0)) {
 				if (cSpeed < maxSpeed) {
@@ -38,6 +57,76 @@ public class BallControll : MonoBehaviour {
 					slider.value = 0;
 				}
 			}
+		}*/
+
+		if (true) {
+			if (isShooting == false) {
+				currentTime -= Time.deltaTime;
+		//		TurnTime = Mathf.Clamp (TurnTime, 0f, Mathf.Infinity);
+				TimeCountDownText.text = string.Format ("{0:00.00}",currentTime);
+			}
+			if (currentTime < 0) { // time is up
+				
+				Turn ();
+				currentTime = TurnTime;
+			}
+				
+			if (Input.GetMouseButton (0) && isShooting == false) {
+				if (cSpeed < maxSpeed) {
+					cSpeed += Time.deltaTime * increase_speed;
+					slider.value += Time.deltaTime * increase_speed;
+				} else {
+					cSpeed = maxSpeed;
+				}
+			} else {
+
+				//ball destory time
+				if (temp > 0) {
+					temp -= Time.deltaTime;
+				}
+				if (temp <= 0 || GameObject.FindGameObjectWithTag("Ball") == null) {
+					isShooting = false;
+				}
+
+
+				if (cSpeed > 0f) {
+					isShooting = true;
+					currentTime = TurnTime;
+					TimeCountDownText.text = string.Format ("{0:00.00}",currentTime);
+
+
+					cSpeed += minSpeed;
+					BallFly.speed = cSpeed;
+
+					temp = Destroy_time;
+
+					Destroy(Instantiate (balls, this.transform.position, this.transform.rotation),Destroy_time);
+
+					Turn ();
+					time = 0f;
+					cSpeed = 0f;
+					slider.value = 0;
+				}
+			}
 		}
+
 	}
+
+	void Turn(){
+		TurnCount--;
+		ArrowRemainText.text = "Remain:" + TurnCount.ToString ();
+		if (TurnCount == 0) { // game end
+			Debug.Log ("Game End -> Result");
+		}
+
+	}
+
+	public void GameStart(){
+		isGameStart = true;
+	}
+
+
+
+
+
 }
--- Assets/Scripts/BallFly.cs
+++ Assets/Scripts/BallFly.cs
@@ -6,6 +6,12 @@ public class BallFly : MonoBehaviour {
 	static public float speed = 0.1f;
 	public Rigidbody rb;
 	// Use this for initialization
+
+	//wind influence 
+	private float WindX = 0; //range from 0.1 to 1 
+	private float WindY = 0; 
+	private float WindZ = 0; 
+
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		rb.AddForce (transform.forward * speed);
@@ -14,8 +20,9 @@ public class BallFly : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
+		rb.AddForce (WindX, WindY, WindZ); //world location 
 	}
+
 	private void OnCollisionEnter(Collision collision){
 		if (collision.gameObject.tag == "Constraint") {
 			Destroy (this.gameObject);
@@ -25,4 +32,12 @@ public class BallFly : MonoBehaviour {
 			Destroy (collision.gameObject);
 		}
 	}
+
+	//destory ball by trigger
+	private void OnTriggerEnter(Collider col){
+		if (col.CompareTag ("Constraint") == true) {
+			Destroy (this.gameObject);
+		}
+	}
+
 }
