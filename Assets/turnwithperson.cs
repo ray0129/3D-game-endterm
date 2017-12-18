373821a53bfcca7814bc429b6bb7c5b519ce4620
Lohan <Lohan@github.com>
2017-12-19 03:35:48 +0800

    turn dir with person

---
+++ Assets/turnwithperson.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnwithperson : MonoBehaviour {
+
+	public Transform target;
+	private float x;
+	private float y;
+	public float Xsens;
+	public float Ysens;
+	public float backwardDis;
+	private Quaternion rotationEuler;
+	private Vector3 camPos;
+
+	// Use this for initialization
+	void Start () {
+		x = 0;
+		y = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x -= Input.GetAxis ("Mouse X") * Xsens * Time.deltaTime;
+		y += Input.GetAxis ("Mouse Y") * Ysens * Time.deltaTime;
+
+		if (x > 360) {
+			x -= 360;
+		} else if (x < 0) {
+			x += 360;
+		}
+		if (y > 100) {
+			y = 100;
+		} else if (y < 100) {
+			y = 100;
+		}
+		rotationEuler = Quaternion.Euler (y, 0, x);
+		camPos = new Vector3 (0,backwardDis,0) + target.position;
+		transform.rotation = rotationEuler;
+		transform.position = camPos;
+	}
+}
